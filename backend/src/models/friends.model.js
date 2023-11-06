ce007046a988b23519bc90d8f45769799270895a
nsyifa <nasywa.syifa.azizah@mail.ugm.ac.id>
2023-11-07 00:27:28 +0900

    added functions for friends table

---
+++ backend/src/models/friends.model.js
@@ -0,0 +1,63 @@
+const db = require("./db.js");
+
+const Friends = function (database) {};
+
+Friends.getUserFriends = (user_id, result) => {
+  db.query(
+    "SELECT * FROM friends, user WHERE user.user_id = friends.friend_user_id AND friends.user_id = ?",
+    [user_id],
+    (err, res) => {
+      if (err) {
+        console.log("error: ", err);
+        result(err, null);
+        return;
+      }
+
+      if (res.length) {
+        console.log("found friends: ", res);
+        result(null, res);
+        return;
+      } else {
+        result({ kind: "not_found" }, null);
+      }
+    }
+  );
+};
+
+Friends.insertFriend = (user_id, friend_user_id, result) => {
+  db.query(
+    "INSERT INTO friends (user_id, friend_user_id) VALUES (?,?) ON DUPLICATE KEY UPDATE user_id = user_id;",
+    [user_id, friend_user_id],
+    (err, res) => {
+      if (err) {
+        console.log("error: ", err);
+        result(err, null);
+        return;
+      } else {
+        console.log("rows inserted");
+        result(null, null);
+        return;
+      }
+    }
+  );
+};
+
+Friends.deleteFriend = (user_id, friend_user_id, result) => {
+  db.query(
+    "DELETE FROM friends where user_id = ? AND friend_user_id = ?;",
+    [user_id, friend_user_id],
+    (err, res) => {
+      if (err) {
+        console.log("error: ", err);
+        result(err, null);
+        return;
+      } else {
+        console.log("rows deleted");
+        result(null, null);
+        return;
+      }
+    }
+  );
+};
+
+module.exports = Friends;
