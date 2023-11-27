02a47f71ca4ee6f942bab83e5dcc38c66f55eb17
nsyifa <nasywa.syifa.azizah@mail.ugm.ac.id>
2023-11-27 21:46:28 +0900

    [intended vul patch] fixed BoF vulnerability

--- backend/src/lib/genderValidator/genderValidator.cc
+++ backend/src/lib/genderValidator/genderValidator.cc
@@ -27,7 +27,7 @@ bool validate(const char *str)
     const regex pattern("^(Male|Female|Other|Prefer not to say)$");
 
    char buffer[BUF_SIZE];
-   strcpy(buffer, str);
+   strncpy(buffer, str, BUF_SIZE);
 
    string gender = buffer;
 
